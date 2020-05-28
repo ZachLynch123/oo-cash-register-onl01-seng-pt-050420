131f0514dd38596184111af32f7f3b4488f5a6d4
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-28 21:40:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize(discount=nil)
+    @total = 0 
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @title = title 
+    @old_totlal = total
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    @discount.to_f 
+    @discount = @discount/100
+    binding.pry
 
+    @total = @total - @discount
+
+    end
+  
+end
